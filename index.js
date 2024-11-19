b023006f4e2a1278de0f9a60dff106a48352c32a
wheredoesyourmindgo <moemanchu@icloud.com>
2024-11-19 10:01:06 -0800

    Remove comma.

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ module.exports.decorateConfig = (config) => {
       },
       .header_header {
         background-color: #efedea;
-      },
+      }
       .header_header .tabs_title {
         color: #2b2822; 
       }
