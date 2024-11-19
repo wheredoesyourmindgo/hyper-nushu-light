b1d142aa9844621f8f2d81c3c00bff6a3922ca1f
Abe Hendricks <ahendricks@pcwa.net>
2024-11-19 08:28:29 -0800

    Setup theme.

--- index.js
+++ index.js
@@ -1,28 +1,46 @@
 module.exports.decorateConfig = (config) => {
   const nushuLightColors = {
-    backgroundColor: '#F8F9FA', // Background
-    foregroundColor: '#333333', // Foreground (Text)
-    cursorColor: '#FF5733',     // Cursor
-    borderColor: '#CCCCCC',     // Border
-    selectionColor: 'rgba(255, 87, 51, 0.3)', // Selection (rgba)
+    foregroundColor: "#2b2822",
+    backgroundColor: "#f8f6f1",
+    selectionColor: "#e8dac0",
+    cursorColor: "#3c7d3f",
+    borderColor: "#d6d4cfe6",
     colors: {
-      black: '#1E1E1E',
-      red: '#FF5733',
-      green: '#28A745',
-      yellow: '#FFC107',
-      blue: '#007BFF',
-      magenta: '#6F42C1',
-      cyan: '#17A2B8',
-      white: '#F8F9FA',
-      lightBlack: '#555555',
-      lightRed: '#FF7961',
-      lightGreen: '#33D17A',
-      lightYellow: '#FFD54F',
-      lightBlue: '#409EFF',
-      lightMagenta: '#AF69EF',
-      lightCyan: '#20C997',
-      lightWhite: '#FFFFFF',
-    }
+      black: "#24292f",
+      red: "#cf222e",
+      green: "#116329",
+      yellow: "#4d2d00",
+      blue: "#0969da",
+      magenta: "#6639ba",
+      cyan: "#1b7c83",
+      white: "#6e7781",
+      lightBlack: "#57606a",
+      lightRed: "#a40e26",
+      lightGreen: "#1a7f37",
+      lightYellow: "#633c01",
+      lightBlue: "#218bff",
+      lightMagenta: "#8250df",
+      lightCyan: "#3192aa",
+      lightWhite: "#8c959f",
+    },
+    css: `
+      * {
+        text-rendering: optimizeLegibility;
+      }
+      .tab_tab {
+        color: #656d76;
+        border-left: 1px solid #d6d4cfe6;
+        background-color: #efedea;
+      }
+      .tab_tab.tab_active {
+        color: #2b2822;
+        border-left: 1px solid #d6d4cfe6;
+        background-color: #f8f6f2;
+      },
+      .header_header {
+        background-color: #efedea;
+      }
+    `,
   };
 
   return Object.assign({}, config, nushuLightColors);
